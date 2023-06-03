8c11d337376478e5ed0fe7dad0e5633f47c179bd
crab <zixxxota@gmail.com>
2023-06-03 14:35:52 +0300

    Initial commit Intro Activity

---
+++ app/src/main/java/com/example/termproject/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+    private Button startButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        startButton=findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LogInActivity.class));
+            }
+        });
+    }
+}
