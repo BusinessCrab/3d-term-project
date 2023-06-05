27ed91df2d086ddf8bd58a5f9cebbe96d6b489bd
crab <zixxxota@gmail.com>
2023-06-05 18:34:21 +0300

    ProfileActivity template

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.termproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity
 {
     private TextView username;
+    private Button profileButton;
 
     @Override
     protected void onCreate(Bundle e)
     {
         super.onCreate(e);
         setContentView(R.layout.activity_main);
+
+        profileButton=findViewById(R.id.profile_button);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), ProfileActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/termproject/ProfileActivity.java
+++ app/src/main/java/com/example/termproject/ProfileActivity.java
@@ -1,4 +1,29 @@
 package com.example.termproject;
 
-public class ProfileActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity
+{
+    private Button backButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        backButton=findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), MainActivity.class));
+                finish();
+            }
+        });
+    }
 }
