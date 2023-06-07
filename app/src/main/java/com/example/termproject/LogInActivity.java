174af8bb8d4ba8f5cc235f1740f0518373b29d3e
crab <zixxxota@gmail.com>
2023-06-07 20:11:56 +0300

    Sign in/Sign up database realization

--- app/src/main/java/com/example/termproject/LogInActivity.java
+++ app/src/main/java/com/example/termproject/LogInActivity.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity{
+    private EditText login, password;
     private Button loginButton, signupButton;
+    private DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginButton=(Button)findViewById(R.id.login_button);
-        signupButton=(Button)findViewById(R.id.signup_button);
+        login = (EditText) findViewById(R.id.login_edittext);
+        password = (EditText) findViewById(R.id.password_edittext);
+        loginButton=(Button) findViewById(R.id.login_button);
+        signupButton=(Button) findViewById(R.id.signup_button);
+        DB = new DBHelper(this);
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,8 +32,21 @@ public class LogInActivity extends AppCompatActivity{
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), MainActivity.class));
-                finish();
+                String log = login.getText().toString();
+                String pass = password.getText().toString();
+
+                if (log.equals("")||pass.equals(""))
+                    Toast.makeText(LogInActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else {
+                     Boolean checkUsernamePassword = DB.checkUsernamePassword(log, pass);
+                     if (checkUsernamePassword == true) {
+                         Toast.makeText(LogInActivity.this, "Log in successful", Toast.LENGTH_SHORT).show();
+                         startActivity(new Intent(v.getContext(), MainActivity.class));
+                         finish();
+                     }
+                     else
+                         Toast.makeText(LogInActivity.this, "Log in failed! Check your login and password!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/termproject/SignUpActivity.java
+++ app/src/main/java/com/example/termproject/SignUpActivity.java
@@ -4,20 +4,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity
 {
-    private Button signupButton2;
+    private EditText login, password, repassword;
+    private Button signupButton;
+    private DBHelper DB;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        signupButton2=findViewById(R.id.signup_button);
-        signupButton2.setOnClickListener(new View.OnClickListener() {
+        login = (EditText) findViewById(R.id.login_edittext);
+        password = (EditText) findViewById(R.id.password_edittext);
+        repassword = (EditText) findViewById(R.id.repassword_edittext);
+        signupButton = (Button) findViewById(R.id.signup_button);
+        DB = new DBHelper(this);
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignUpActivity.this, LogInActivity.class));
+                String log = login.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(log.equals("")||pass.equals("")|| repass.equals(""))
+                    Toast.makeText(SignUpActivity.this, "Please enter all the fields", Toast.LENGTH_LONG).show();
+                else{
+                    if (pass.equals(repass))
+                    {
+                        Boolean checkLogin = DB.checkUsername(log);
+                        if (checkLogin == false)
+                        {
+                            Boolean insert = DB.insertData(log, pass);
+                            if (insert==true) {
+                                Toast.makeText(SignUpActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(v.getContext(), LogInActivity.class));
+                                finish();
+                            }
+                            else
+                                Toast.makeText(SignUpActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                            Toast.makeText(SignUpActivity.this, "This user already exists", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                        Toast.makeText(SignUpActivity.this, "Passwords confirmation failed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
